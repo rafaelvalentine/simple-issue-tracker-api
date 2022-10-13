d9bde5d445081dbb415d2964790ea2f3b4c72e7f
ChrisEzikeJL <chris@joint.cool>
2022-10-13 04:54:47 +0100

    fix: added is_completed to serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :description, :is_active
+  attributes :id, :title, :description, :is_completed, :is_active
   has_one :sprint
 end
